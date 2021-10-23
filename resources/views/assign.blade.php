47e700c06b0f8cf2469dc6b8b0e978e708378bee
Andrea Maurice <andreamauricedg@gmail.com>
2021-10-23 16:19:26 +0800

    assign

--- resources/views/assign.blade.php
+++ resources/views/assign.blade.php
@@ -8,32 +8,49 @@
             body{
                 background-color: #EDEDED;
             }
-            .assign-crew{
+            .assign-crew, .assign-ship{
                 background-color: white;
                 margin: 50px;
                 padding: 20px;
+            }
+            .collect{
+                display: block;
             }
         </style>
     </head>
     <body>
         <div class="assign-crew">
+            <form action="POST">
+                <div class="title">
+                    <h2>Crew Information</h2>
+                </div>
+                <div class="collect">
+                <p>Name:</p> 
+                    <input type="text" >
+                <p>Assign Rank:</p> 
+                    <select name="" id="">
+                        <option value=""></option>
+                    </select>
+                </div>
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+
+        <div class="assign-ship">
             <div class="title">
-                <h2>Crew Information</h2>
+                <h2>Ship Information</h2>
             </div>
-            <div>
-                Name:
+            <div class="collect">
+               <p>Ship Name:</p> 
                 <input type="text" >
-                Assign Rank:
+               <p>Assign Crews:</p> 
                 <select name="" id="">
                     <option value=""></option>
                 </select>
+                <p>Speed Class</p>
+                <input type="text" >
             </div>
-            
-            
-
-
-
-
+            <input type="submit" value="Submit" />
         </div>
     </body>
 </html>
