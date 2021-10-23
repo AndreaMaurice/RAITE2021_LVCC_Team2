3a34a7c80e91149acaae7572fe5c82ab29ea244e
Andrea Maurice <andreamauricedg@gmail.com>
2021-10-23 16:02:58 +0800

    assign page

---
+++ resources/views/assign.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <style>
+            body{
+                background-color: #EDEDED;
+            }
+            .assign-crew{
+                background-color: white;
+                margin: 50px;
+                padding: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="assign-crew">
+            <div class="title">
+                <h2>Crew Information</h2>
+            </div>
+            <div>
+                Name:
+                <input type="text" >
+                Assign Rank:
+                <select name="" id="">
+                    <option value=""></option>
+                </select>
+            </div>
+            
+            
+
+
+
+
+        </div>
+    </body>
+</html>
