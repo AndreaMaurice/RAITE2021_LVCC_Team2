b45c6a367be02d09c384a2e24a219a7ed8f1e53b
djsaballa <danieljohnsaballa@student.laverdad.edu.ph>
2021-10-23 14:07:26 +0800

    modelsssss

--- app/Http/Controllers/AgencyController.php
+++ app/Http/Controllers/AgencyController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+Use App\Models\Ships;
+Use App\Models\Crewmates;
+Use App\Models\Crew;
+
 
 class AgencyController extends Controller
 {
